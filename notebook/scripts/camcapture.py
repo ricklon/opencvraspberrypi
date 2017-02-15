6f99f2d12a6c0e78068440347c1b5fe72011502c
Ricklon <rick.rickanderson@gmail.com>
2017-02-14 20:49:26 -0500

    Added much

--- notebook/scripts/camcapture.py
+++ notebook/scripts/camcapture.py
@@ -6,12 +6,13 @@ cap = cv2.VideoCapture(0)
 while(True):
     #capture frame by frame
     ret, frame = cap.read()
+
+    if ret: 
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    cv2.imshow('frame', gray)
-    if cv2.waitKey(1) &0xFF == ord('q'):
-        break
+        cv2.imshow('frame', gray)
+#    if cv2.waitKey(1) &0xFF == ord('q'):
+#        break
         
 cap.release();
 cv2.destroyAllWindows()
